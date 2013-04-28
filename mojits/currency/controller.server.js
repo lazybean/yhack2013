53ac332a5597ecfa5624b80169adcf16e14dc9d6
Ludovic Fardel <ludovic.fardel@gmail.com>
2013-04-28 04:43:26 +0100

    ui changes

---
+++ mojits/currency/controller.server.js
@@ -0,0 +1,40 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('currency', function(Y, NAME) {
+
+/**
+ * The currency module.
+ *
+ * @module currency
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            ac.models.get('currencyModelFoo').getData(function(err, data) {
+                if (err) {
+                    ac.error(err);
+                    return;
+                }
+                ac.assets.addCss('./index.css');
+                ac.done({
+                    status: 'Mojito is working.',
+                    data: data
+                });
+            });
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-assets-addon', 'mojito-models-addon', 'currencyModelFoo']});
--- yui_modules/yootravel-yql-fmt.common.js
+++ yui_modules/yootravel-yql-fmt.common.js
@@ -3,7 +3,8 @@ YUI.add('yootravel-yql-fmt', function (Y) {
     getWarningUK : "select * from html where url=\"https://www.gov.uk/foreign-travel-advice/{country}\" and xpath='//div[@class=\"inner\"]/p'",
     getVisa: "select * from html where url='http://china.visahq.com/requirements/{country}' and xpath='//div[@class=\"visa_info_req\"]/dl[@class!=\"\"]'",
     getBossImage: "select * from boss.search where q=\"{city} {country} -map\" and ck=\"dj0yJmk9YWF3ODdGNWZPYjg2JmQ9WVdrOWVsWlZNRk5KTldFbWNHbzlNVEEyTURFNU1qWXkmcz1jb25zdW1lcnNlY3JldCZ4PTUz\" and secret=\"a3d93853ba3bad8a99a175e8ffa90a702cd08cfa\" and service=\"images\" and count=2",
-    getWeather: "select item.description from weather.forecast where {woeid}"
+    getWeather: "select item.description from weather.forecast where {woeid}",
+    getEANLandmark: "http://api.ean.com/ean-services/rs/hotel/v3/geoSearch?cid=55505&apiKey=e89mfmwrj3xhtvjnemeq4t9q&destinationString={city}&type=2"
   };
 
 }, '0.0.1', {requires: []});
